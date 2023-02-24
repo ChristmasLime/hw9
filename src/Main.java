48d2e22443d1c3bcc477ef6d0cbf8238179bf1d8
ChristmasLime <christmasboom@mail.ru>
2023-02-24 12:35:10 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,53 @@ public class Main {
         task4();
 
     }
-
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void task1() {
         System.out.println("Задача 1");
-
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int element:arr){
+            sum+=element;
+        }
+        System.out.println("Сумма трат за месяц составила "+sum+" рублей");
     }
-
     public static void task2() {
         System.out.println("Задача 2");
-
+        int[] arr = generateRandomArray();
+        int max = 0;
+        int min = 200000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }if (arr[i] < min){
+                min=arr[i];
+            }
+        }System.out.println("Минимальная сумма трат за день составила "+min+ " рублей.\nМаксимальная сумма трат за день составила "+max+  " рублей.");
     }
-
     public static void task3() {
         System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        float sum = 0;
+        float day = 30;
+        for (int element:arr){
+            sum+=element;
 
+        }sum=sum/day;
+        System.out.println("Средняя сумма трат за месяц составила " +sum+" рублей.");
     }
-
     public static void task4() {
         System.out.println("Задача 4");
-
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1; i>=0 ;i--){
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
     }
 }
